6a1682ebdad2ee48e28b4ecc5d4d3c2de93a5ee5
DianaBusyk <diana.busyk@gmail.com>
2023-05-04 23:41:05 +0300

    first commit

--- example/App.vue
+++ example/App.vue
@@ -1,78 +1,26 @@
 <template>
   <div id="app">
-    <a class="github-fork-ribbon" href="https://github.com/xiaokaike/vue-color" title="Fork me on GitHub">Fork me on GitHub</a>
     <div class="header-container">
       <div class="header-bg" :style="{'background-color': bgc}"></div>
       <div class="header">
-        <div class="intro-wrap">
-          <div class="intro">
-            <h1>Vue-color</h1>
-            <p role="presentation">A Collection of Color Pickers from Sketch, Photoshop, Chrome, Github, Twitter, Material Design & more</p>
-          </div>
-        </div>
+       
         <div class="demo-item">
           <chrome-picker :value="colors" @input="updateValue"></chrome-picker>
-          <h2>Chrome</h2>
+          
         </div>
       </div>
     </div>
 
-    <div class="demo-container">
-      <div class="demo-list">
-        <div class="demo-item">
-          <sketch-picker v-model="colors"></sketch-picker>
-          <h2>Sketch</h2>
-        </div>
-        <div class="demo-item">
-          <photoshop-picker v-model="colors" @ok="onOk" @cancel="onCancel"></photoshop-picker>
-          <h2>Photoshop</h2>
-        </div>
-      </div>
-      <div class="demo-list">
-        <div class="demo-item">
-          <material-picker v-model="colors"></material-picker>
-          <h2>Material</h2>
-        </div>
-        <div class="demo-item">
-          <slider-picker v-model="colors"></slider-picker>
-          <h2>Slider</h2>
-        </div>
-      </div>
-      <div class="demo-list">
-        <div class="demo-item">
-          <compact-picker v-model="colors"></compact-picker>
-          <h2>Compact</h2>
-          <br>
-          <br>
-          <grayscale-picker :value="colors" @input="updateValue"></grayscale-picker>
-          <h2>Grayscale</h2>
-        </div>
-        <div class="demo-item">
-          <swatches-picker v-model="colors"></swatches-picker>
-          <h2>Swatches</h2>
-        </div>
-      </div>
-      <div class="demo-list">
-        <div class="demo-item">
-          <twitter-picker :value="colors" @input="updateValue"></twitter-picker>
-          <h2>Twitter</h2>
-        </div>
-      </div>
-
-    </div>
+   
   </div>
 </template>
-
 <script>
-import material from '../src/components/Material.vue'
-import compact from '../src/components/Compact.vue'
-import grayscale from '../src/components/Grayscale.vue'
-import swatches from '../src/components/Swatches.vue'
-import slider from '../src/components/Slider.vue'
-import sketch from '../src/components/Sketch.vue'
+
+
+
+
 import chrome from '../src/components/Chrome.vue'
-import photoshop from '../src/components/Photoshop.vue'
-import twitter from '../src/components/Twitter.vue'
+
 
 let defaultProps = {
   hex: '#194d33e6',
@@ -99,15 +47,8 @@ let defaultProps = {
 
 export default {
   components: {
-    'material-picker': material,
-    'compact-picker': compact,
-    'grayscale-picker': grayscale,
-    'swatches-picker': swatches,
-    'slider-picker': slider,
-    'sketch-picker': sketch,
     'chrome-picker': chrome,
-    'photoshop-picker': photoshop,
-    'twitter-picker': twitter
+    
   },
   data () {
     return {
@@ -120,12 +61,6 @@ export default {
     }
   },
   methods: {
-    onOk () {
-      console.log('ok')
-    },
-    onCancel () {
-      console.log('cancel')
-    },
     updateValue (value) {
       this.colors = value
     }
